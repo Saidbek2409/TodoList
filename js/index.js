79ca34f84ebf4f5fd6a3e2dde81280c6a49cd746
Saidbek <106152417+Saidbek2409@users.noreply.github.com>
2023-05-20 09:12:47 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('form');
+const textInput = document.getElementById('text');
+const blocksContainer = document.getElementById('blocks');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const text = textInput.value;
+  const time = new Date().toLocaleTimeString();
+  
+  const block = document.createElement('div');
+  block.classList.add('block');
+  block.style.gap = '10px'
+  block.style.alignItems = 'center'
+
+  const textElement = document.createElement('p');
+  textElement.textContent = text;
+  block.appendChild(textElement);
+  
+  const timeElement = document.createElement('span');
+  timeElement.classList.add('time');
+  timeElement.textContent = time;
+  block.appendChild(timeElement);
+  
+  blocksContainer.appendChild(block);
+  
+  textInput.value = '';
+});
